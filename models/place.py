f84d2ff1ea9248dc37dbfddbbf9d4565dd73eec9
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-25 11:34:16 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+import Base
+from  sqlalchemy import *
 
-
+place_amenity = Table('place_amenity',Base.metadata,
+                      Column('place_id',String(60),ForeignKey("places.id"),primary_key=True,nullable=False),
+                      Column('amenity_id',String(60),ForeignKey("amenities.id",primarykey=True,nullable=False),
+                     )
 class Place(BaseModel):
     """ A place to stay """
     city_id = ""
