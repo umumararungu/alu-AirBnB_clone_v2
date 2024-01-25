086e4d73599697bafbbe617199467d79e83b821d
KennyKvn007 <k.mugisha@alustudent.com>
2024-01-25 10:48:34 +0200

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- A script for MySQL serve test file for the project 
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES
