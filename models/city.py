3eb3c3a19de4e6e2e738387427b2fa8297a08295
KennyKvn007 <k.mugisha@alustudent.com>
2024-01-25 13:01:24 +0200

    DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """The city class, contains state ID and name"""
 
     __tablename__ = "cities"
