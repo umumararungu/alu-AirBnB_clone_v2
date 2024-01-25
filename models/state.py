051ff02c01aec871eba48456fbeedee7b48995a6
KennyKvn007 <k.mugisha@alustudent.com>
2024-01-25 13:07:06 +0200

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -3,6 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.schema import ForeignKey
+from os import getenv
+from .city import City
 
 
 class State(BaseModel, Base):
@@ -11,3 +14,18 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete-orphan", backref="state")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+
+        @property
+        def cities(self):
+            """Getter for list of all `City` objects when in file storage
+            mode.
+            """
+            from . import storage
+
+            cities = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
