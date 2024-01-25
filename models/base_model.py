3c35bc8fbda7fec9ff20400a6195c0c18a338b65
KennyKvn007 <k.mugisha@alustudent.com>
2024-01-25 12:53:12 +0200

    DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,11 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 
 
-Base = declaractive_base()
+Base = declarative_base()
 
 
 class BaseModel:
