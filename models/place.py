8288d5bc72c09f998781bbd18cacbf628c6a04d2
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-26 09:45:56 +0200

    correct

--- models/place.py
+++ models/place.py
@@ -17,14 +17,14 @@ class Place(BaseModel):
        amenities = relationship('Amenity',secondary='place_amenity',
                                 backref="place_amenities",viewonly=False)
     else:
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
