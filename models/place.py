7535d785633a372e9b6fa904ade77cb05a651cfe
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-26 09:50:41 +0200

    task

--- models/place.py
+++ models/place.py
@@ -3,27 +3,25 @@
 from models.base_model import BaseModel,Base
 from  sqlalchemy import *
 from sqlalchemy.orm import relationship
-
-if models.storage_t == 'db':  
-  place_amenity = Table('place_amenity',Base.metadata,
+ 
+place_amenity = Table('place_amenity',Base.metadata,
                         Column('place_id',String(60),ForeignKey("places.id"),primary_key=True,nullable=False),
                         Column('amenity_id',String(60),ForeignKey("amenities.id",primarykey=True,nullable=False)),
                        )
 class Place(BaseModel):
     """ A place to stay """
 
-    if models.storage_t == 'db':
-       amenities = relationship('Amenity',secondary='place_amenity',
+    amenities = relationship('Amenity',secondary='place_amenity',
                                 backref="place_amenities",viewonly=False)
-    else:
-        city_id = ""
-        user_id = ""
-        name = ""
-        description = ""
-        number_rooms = 0
-        number_bathrooms = 0
-        max_guest = 0
-        price_by_night = 0
-        latitude = 0.0
-        longitude = 0.0
-        amenity_ids = []
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
