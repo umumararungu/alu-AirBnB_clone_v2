e77794a670edc770e20eda170c84cecdf9044f2c
umumararungu <umucynthi@gmail.com>
2024-01-26 03:42:36 -0800

    create

--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
@@ -10,18 +10,22 @@ from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
 from .city import City
 
-    Base = declarative_base()
+Base = declarative_base()
+
 
 class State(BaseModel, Base):
     """State class"""
 
-    engine = Create_engine('mysql://hbnb_dev:hbnb_dev_pwd@localhost/hbnb_dev_db')
+    engine = Create_engine(
+        'mysql://hbnb_dev:hbnb_dev_pwd@localhost/hbnb_dev_db')
     session = sessionmaker(bind=engine)()
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan", backref="state")
-    
+    cities = relationship(
+        "City",
+        cascade="all, delete-orphan",
+        backref="state")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
 
