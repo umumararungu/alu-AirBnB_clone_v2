47ab11baacedc474e7097442a3a1cd1834094bdf
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-26 09:46:47 +0200

    base

--- models/place.py
+++ models/place.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-import Base
+from models.base_model import BaseModel,Base
 from  sqlalchemy import *
 from sqlalchemy.orm import relationship
 
