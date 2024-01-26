f85ad92d3ab19c4a0cba60aff7c93855b1c0210a
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-26 13:19:07 +0200

    state

--- models/state.py
+++ models/state.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from sqlalchemy import Create_engine
+from sqlalchemy.orm import sessionmaker
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
 from .city import City
+
+engine = Create_engine('mysql://hbnb_dev:hbnb_dev_pwd@localhost/hbnb_dev_db')
+session = sessionmaker(bind=engine)()
+
+Base = declarative_base()
 
 
 class State(BaseModel, Base):
