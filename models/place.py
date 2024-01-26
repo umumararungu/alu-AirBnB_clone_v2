cfc3ba57f5c88abddeeb68d02a44e877dd7190a1
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-01-26 09:54:58 +0200

    comment

--- models/place.py
+++ models/place.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel,Base
-from  sqlalchemy import *
+# from  sqlalchemy import *
 # from sqlalchemy.orm import relationship
  
-place_amenity = Table('place_amenity',Base.metadata,
-                        Column('place_id',String(60),ForeignKey("places.id"),primary_key=True,nullable=False),
-                        Column('amenity_id',String(60),ForeignKey("amenities.id",primarykey=True,nullable=False)),
-                       )
+# place_amenity = Table('place_amenity',Base.metadata,
+#                         Column('place_id',String(60),ForeignKey("places.id"),primary_key=True,nullable=False),
+#                         Column('amenity_id',String(60),ForeignKey("amenities.id",primarykey=True,nullable=False)),
+#                        )
 class Place(BaseModel):
     """ A place to stay """
 
-    amenities = relationship('Amenity',secondary='place_amenity',
-                                backref="place_amenities",viewonly=False)
+    # amenities = relationship('Amenity',secondary='place_amenity',
+    #                             backref="place_amenities",viewonly=False)
 
     city_id = ""
     user_id = ""
