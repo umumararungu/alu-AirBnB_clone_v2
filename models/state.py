a470dcecbe967374b600e4b253e6d4ebb0b53675
KennyKvn007 <k.mugisha@alustudent.com>
2024-01-26 13:10:15 +0200

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy.schema import ForeignKey
 from os import getenv
 from .city import City
 
