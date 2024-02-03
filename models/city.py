ab9103cdc8386e9168c2b784c7db9d8519bba299
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-02-03 10:40:24 +0200

    os

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the city class"""
+import os
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
