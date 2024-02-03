1da4bdda4455d38678e7a839648ef6f4349ab154
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-02-03 10:35:15 +0200

    city

--- models/city.py
+++ models/city.py
@@ -18,7 +18,7 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     id = Column(String(60), nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    # places = relationship(
-    #     "Place", cascade="all, delete, delete-orphan", backref="cities"
-    # )
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    places = relationship(
+        "Place", cascade="all, delete, delete-orphan", backref="cities"
+    )
