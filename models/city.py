b4889569911c2ff1aadea587df55ea031bb9b7b6
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-06 12:57:10 +0200

    DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """This is the city class"""
 from sqlalchemy.ext.declarative import declarative_base
-from base_model import BaseModel, Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class City(BaseModel, Base):
--- models/state.py
+++ models/state.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """This is the state class"""
-import os
-from models import storage
-from base_model import BaseModel, Base
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
 import shlex
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 
 class State(BaseModel, Base):
@@ -18,15 +17,11 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = (
-        relationship("City", cascade="all, delete, delete-orphan", backref="state")
-        if os.getenv("HBNB_TYPE_STORAGE") == "db"
-        else ""
-    )
+    cities = relationship("City", cascade="all, delete, delete-orphan", backref="state")
 
     @property
     def cities(self):
-        var = storage.all()
+        var = models.storage.all()
         lista = []
         result = []
         for key in var:
@@ -38,9 +33,3 @@ class State(BaseModel, Base):
             if elem.state_id == self.id:
                 result.append(elem)
         return result
-
-
-engine = create_engine("mysql+mysqldb://hbnb_dev:hbnb_dev_pwd/hbnb_dev_db@localhost")
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
