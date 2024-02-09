5e003a16e9dd1aa31aea04674155838b02d424c4
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-09 11:02:30 +0200

    bug free

--- tests/test_console.py
+++ tests/test_console.py
@@ -33,13 +33,13 @@ class TestConsoleDocs(unittest.TestCase):
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
         self.assertIsNot(console.__doc__, None, "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1, "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
         self.assertIsNot(
             HBNBCommand.__doc__, None, "HBNBCommand class needs a docstring"
         )
-        self.assertTrue(
-            len(HBNBCommand.__doc__) >= 1, "HBNBCommand class needs a docstring"
-        )
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
