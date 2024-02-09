429a6b13836c41f80c94a6361233a7e5bdb2939b
KennyKvn007 <k.mugisha@alustudent.com>
2024-02-09 11:07:27 +0200

    bug free

--- tests/test_console.py
+++ tests/test_console.py
@@ -27,19 +27,19 @@ class TestConsoleDocs(unittest.TestCase):
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(["tests/test_console.py"])
         self.assertEqual(
-            result.total_errors, 2, "Found code style errors (and warnings)."
+            result.total_errors, 0, "Found code style errors (and warnings)."
         )
 
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
         self.assertIsNot(console.__doc__, None, "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1, "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
         self.assertIsNot(
             HBNBCommand.__doc__, None, "HBNBCommand class needs a docstring"
         )
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs a docstring")
+        self.assertTrue(
+            len(HBNBCommand.__doc__) >= 1, "HBNBCommand class needs a docstring"
+        )
