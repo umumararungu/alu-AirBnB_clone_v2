08e529cd3f60c5f2181f6c64172d4fffc378bd15
Mpano Umumararungu Cynthia <127386030+umumararungu@users.noreply.github.com>
2024-02-15 14:39:59 +0200

    old

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,42 +1,54 @@
 #!/usr/bin/python3
+"""
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
 
-import unittest
-import MySQLdb
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
+"""
 
-class TestDatabaseFunctionality(unittest.TestCase):
-    def setUp(self):
-        """
-        Connect to the test database
-        """
-        self.conn = MySQLdb.connect(
-            user='hbnb_test',
-            password='hbnb_test_pwd',
-            host='localhost',
-            database='hbnb_test_db'
-        )
-        self.cursor = self.conn.cursor()
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
+env.hosts = ['54.226.156.77', '54.83.75.201']
 
-    def tearDown(self):
-       """
-       Close the database connection
-       """
-        self.cursor.close()
-        self.conn.close()
 
-    def test_create_state(self):
-        """
-        Get the initial number of records in the states table
-        """
-        self.cursor.execute("SELECT COUNT(*) FROM states")
-        initial_count = self.cursor.fetchone()[0]
-        
-        create_state_function()  
-        
-        self.cursor.execute("SELECT COUNT(*) FROM states")
-        final_count = self.cursor.fetchone()[0]
-        
-        self.assertEqual(final_count, initial_count + 1, "New state was not created")
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
 
-if __name__ == '__main__':
-    unittest.main()
 
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
