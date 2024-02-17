5c1e7bd7998e5a4cf53f68839893639f0c996b0e
umumararungu <umcynthi@gmail.com>
2024-02-17 09:48:52 +0200

    done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,9 +21,8 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def C_fun(text):
     """C page"""
-    text = escape(text.replace('_', ' '))
 
-    return f'C,{text}'
+    return f'C,{escape(text)}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
