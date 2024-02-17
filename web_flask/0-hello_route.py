042b6e0e2aac5a5b4ff8b99589f90e2cf3abfc2e
umumararungu <umcynthi@gmail.com>
2024-02-17 09:14:10 +0200

    donee

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    """
+    first page
+    """
+    return "<p>Hello HBNB!</p>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
