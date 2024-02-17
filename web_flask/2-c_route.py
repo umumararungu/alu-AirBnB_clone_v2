f8721bb2536d92af5f27d8beec0aceaee2191968
umumararungu <umcynthi@gmail.com>
2024-02-17 09:33:07 +0200

    ccc

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""second task"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """first page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """second page"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_fun(text):
+    """third page"""
+    return "C,{escape(name)}!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
