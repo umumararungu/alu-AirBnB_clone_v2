1f32e72bd0244884996c5c790af1a3de7d9539b6
umumararungu <umcynthi@gmail.com>
2024-02-18 16:11:18 +0200

    return

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,8 +27,10 @@ def C_fun(text):
 @app.route("/python/<text>", strict_slashes=False)
 def Python_fun(text):
     """C page"""
-    text = "is cool"
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+    if text == 0 :
+        return 'Python is cool'
+    else:
+        text = text.replace('_', ' ')
+        return 'Python {}'.format(text)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
