2bfec90505a923c4ca0d01ccf1a3b8db356a6ce2
umumararungu <umcynthi@gmail.com>
2024-02-18 16:24:51 +0200

    numbers

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""second task"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """first page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """second page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_fun(text):
+    """C page"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def Python_fun(text="is cool"):
+    """ Python page"""
+
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def numbers(n):
+    """Number page"""
+    if isinstance(n, int):
+        return '{} is a number'.format(n)
+    else:
+        return False
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
