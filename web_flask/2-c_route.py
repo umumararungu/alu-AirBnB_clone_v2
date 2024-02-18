15078b2553cef57f785567c1608fe2f2aed1fc1d
umumararungu <umcynthi@gmail.com>
2024-02-18 15:29:59 +0200

    savee

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """second task"""
 from flask import Flask
-from markupsafe import escape
 
 
 app = Flask(__name__)
@@ -23,7 +22,7 @@ def hbnb():
 def C_fun(text):
     """C page"""
 
-    return f"C, {text}!"
+    # return f"C, {text}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
