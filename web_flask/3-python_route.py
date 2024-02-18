fe7bd9054d435fee3cabdf502c74e2914cd6d5e8
umumararungu <umcynthi@gmail.com>
2024-02-18 15:51:18 +0200

    python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""second task"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """first page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """second page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_fun(text):
+    """C page"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+@app.route("/python/<text>", strict_slashes=False)
+def Python_fun(text):
+    """C page"""
+    text = "is cool"
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
