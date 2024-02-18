81c47f5ce17629925963e2cb0a3c0c867681fdc0
umumararungu <umcynthi@gmail.com>
2024-02-18 17:22:40 +0200

    checking

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,9 +51,9 @@ def odd_even(n):
     """Html page"""
     if n % 2 == 0:
         return render_template('6-number_odd_or_even.html',
-                             number=n, check="even")
+                                number=n, check="even")
     elif n % 2 != 0:
         return render_template('6-number_odd_or_even.html',
-                             number=n, check="odd")
+                               number=n, check="odd")
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
